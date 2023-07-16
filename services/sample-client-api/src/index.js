8c7576ddfa8b87c20a2609cfffd924bac736ad6d
danielpakos <daniel@spyre.digital>
2023-07-16 16:26:42 +0100

    test 9

--- services/sample-client-api/src/index.js
+++ services/sample-client-api/src/index.js
@@ -5,7 +5,7 @@ const host = process.env['APP_HOST'] ?? '127.0.0.1'
 server()
     .then((app) => {
         app.listen({ port: port, host: host })
-            // test #8
+            // test #9
             // .then(async () => {
             //     await app.ready()
             //     app.swagger()
