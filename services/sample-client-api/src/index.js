dec3460800ed86ff0741cc059a8a09aa7c7b335a
danielpakos <daniel@spyre.digital>
2023-07-16 19:24:42 +0100

    run tests for only changed files #5

--- services/sample-client-api/src/index.js
+++ services/sample-client-api/src/index.js
@@ -5,7 +5,7 @@ const host = process.env['APP_HOST'] ?? '127.0.0.1'
 server()
     .then((app) => {
         app.listen({ port: port, host: host })
-            // test #13
+            // test #14
             // .then(async () => {
             //     await app.ready()
             //     app.swagger()
