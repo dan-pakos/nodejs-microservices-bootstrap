e3bf8a2e32c4d1ff1fc556bf83e4e318ef4d1dd7
danielpakos <daniel@spyre.digital>
2023-07-16 16:35:08 +0100

    test 2.3

--- services/sample-client-api/src/app.js
+++ services/sample-client-api/src/app.js
@@ -42,7 +42,7 @@ const server = async () => {
                 description: 'API documentation for sample-client-api',
                 version: fast.config.envs.VERSION,
             },
-            // TODO TEST 2.2
+            // TODO TEST 2.3
             // externalDocs: {
             //     url: 'https://swagger.io',
             //     description: 'Find more info here',
