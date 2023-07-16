4c166e0b1edea4513c4126b8ab7831c7c12ff4ec
danielpakos <daniel@spyre.digital>
2023-07-16 14:05:26 +0100

    update protobufjs package

---
+++ packages/grpc-provider/dist/index.d.ts
@@ -0,0 +1,19 @@
+import * as grpc from "@grpc/grpc-js";
+export default class GrpcProvider {
+    #private;
+    set service(serviceName: string);
+    get service(): string;
+    constructor(connectionSettings: ConnectionSettings, protoSettings: ProtoSettings);
+    connect(): void;
+    invoke(methodName: string, data: any): Promise<unknown>;
+}
+interface ProtoSettings {
+    path: string;
+    package: string;
+    service?: string;
+}
+interface ConnectionSettings {
+    address: string;
+    creds?: grpc.ChannelCredentials;
+}
+export { GrpcProvider };
