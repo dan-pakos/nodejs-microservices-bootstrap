f9807a83ffbd9f3d64c49748b071aafb55184234
dpakos <dpakos@polymatic.software>
2025-08-10 12:51:58 +0100

    Fix prettier and format 4

--- services/sample-service-a/src/app.ts
+++ services/sample-service-a/src/app.ts
@@ -3,11 +3,11 @@ import configPlugin from './plugins/config/config-plugin.js'
 import kafkaPlugin from './plugins/kafka/kafka-plugin.js'
 
 const app = () => {
-    return {
-        config: configPlugin(),
-        mongo: mongoPlugin,
-        kafka: kafkaPlugin,
-    }
+  return {
+    config: configPlugin(),
+    mongo: mongoPlugin,
+    kafka: kafkaPlugin,
+  }
 }
 
 export default app
